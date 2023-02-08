4812ac9599e291554f22e59577fea1b5640b5610
billnairk <billnairk@gmail.com>
2023-02-08 14:25:17 +0900

    4.1

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-6 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between">
+          <span className="text-gray-500 my-2">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="mt-5 text- bg-blue-500 text-white p-4 rounded-xl text-center w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
+};
+
+export default Home;
