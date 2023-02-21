b51d399425699054ef665b7eac73a7fd9ef2fe41
billnairk <billnairk@gmail.com>
2023-02-21 15:32:28 +0900

    5.12

---
+++ pages/profile/edit.tsx
@@ -0,0 +1,50 @@
+import { NextPage } from "next";
+
+const EditProfile: NextPage = () => {
+  return (
+    <div className="py-10 px-4 space-y-4">
+      <div className="flex space-x-4">
+        <div className="w-12 h-12 bg-purple-400 rounded-full" />
+        <label
+          htmlFor="picture"
+          className="flex items-center text-sm text-gray-500 font-medium my-2 px-3 rounded-md border border-gray-500 hover:border-purple-400 hover:text-purple-300"
+        >
+          Change
+          <input id="picture" type="file" className="hidden" accept="image/*" />
+        </label>
+      </div>
+      <div className="space-y-1">
+        <label htmlFor="email" className="text-sm text-gray-500 font-bold">
+          Email address
+        </label>
+        <input
+          id="email"
+          type="email"
+          required
+          className="appearance-none shadow-md my-2 w-full rounded-md border-gray-400 focus:ring-purple-300 focus:border-purple-300"
+        />
+      </div>
+      <div className="space-y-1">
+        <label htmlFor="email" className="text-sm text-gray-500 font-bold">
+          Email address
+        </label>
+        <div className="flex shadow-md my-2">
+          <span className="flex items-center justify-center bg-gray-100 select-none border-gray-400 rounded-l-md border border-r-0 px-2 text-sm text-gray-500">
+            +82
+          </span>
+          <input
+            id="input"
+            type="number"
+            required
+            className="appearance-none w-full rounded-r-md focus:ring-purple-300 border-gray-400 focus:border-purple-300"
+          />
+        </div>
+      </div>
+      <button className="bg-purple-600 w-full p-4 rounded-md shadow-md text-white hover:font-bold hover:text-md transition-all">
+        Update profile
+      </button>
+    </div>
+  );
+};
+
+export default EditProfile;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -4,7 +4,7 @@ const Profile: NextPage = () => {
   return (
     <div className="py-10 px-4">
       <div className="flex items-center space-x-3">
-        <div className="w-12 h-12 bg-purple-400 rounded-full" />
+        <div className="w-16 h-16 bg-purple-400 rounded-full" />
         <div className="flex flex-col">
           <span className="font-medium text-gray-900">Steve Jebs</span>
           <span className="text-sm text-gray-700">Edit profile &rarr;</span>
