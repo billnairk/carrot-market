c5c91efb00cff8d460211e2d621170b02ade9b60
billnairk <billnairk@gmail.com>
2023-02-24 14:49:03 +0900

    COMPOTNENTS - PROFILE

--- pages/profile/bought.tsx
+++ pages/profile/bought.tsx
@@ -1,62 +1,24 @@
+import Item from "@/components/item";
+import Layout from "@/components/layout";
 import { NextPage } from "next";
 
 const Bought: NextPage = () => {
   return (
-    <div>
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex justify-between p-4 border-b border-gray-400"
-        >
-          <div className="flex space-x-4">
-            <div className="w-24 h-24 bg-slate-400 rounded-md" />
-            <div className="flex flex-col pt-2">
-              <h3 className="font-bold">New iPhone 14</h3>
-              <span className="text-sm text-gray-500">Black</span>
-              <span className="pt-2 font-medium hover:font-black transition-all">
-                $95
-              </span>
-            </div>
-          </div>
-          <div className="flex items-end space-x-2 text-gray-600">
-            <div className="flex items-center space-x-0.5">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-              <span>1</span>
-            </div>
-            <div className="flex items-center space-x-0.5">
-              <svg
-                className="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              <span>1</span>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Layout canGoBack title="구매내역">
+      <div>
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            key={i}
+            id={i}
+            title="New iPhone 14"
+            color="Black"
+            price="$95"
+            hearts={1}
+            comments={1}
+          />
+        ))}
+      </div>
+    </Layout>
   );
 };
 
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,75 +1,86 @@
 import Layout from "@/components/layout";
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Profile: NextPage = () => {
   return (
     <Layout hasTabBar title="My">
       <div className="py-4 px-4">
         <div className="flex items-center space-x-3">
-          <div className="w-16 h-16 bg-purple-400 rounded-full" />
+          <div className="w-16 h-16 bg-purple-400 rounded-full hover:bg-purple-600 transition-all" />
           <div className="flex flex-col">
             <span className="font-medium text-gray-900">Steve Jebs</span>
-            <span className="text-sm text-gray-700">Edit profile &rarr;</span>
+            <Link href="/profile/edit">
+              <span className="text-sm text-gray-700 hover:font-medium transition-all">
+                Edit profile &rarr;
+              </span>
+            </Link>
           </div>
         </div>
         <div className="mt-10 flex justify-around">
-          <div className=" flex flex-col items-center">
-            <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                ></path>
-              </svg>
+          <Link href="/profile/sold">
+            <div className=" flex flex-col items-center">
+              <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white hover:bg-purple-700 transition-all">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  ></path>
+                </svg>
+              </div>
+              <span className="text-sm text-gray-700">판매내역</span>
             </div>
-            <span className="text-sm text-gray-700">판매내역</span>
-          </div>
-          <div className=" flex flex-col items-center">
-            <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                ></path>
-              </svg>
+          </Link>
+          <Link href="/profile/bought">
+            <div className=" flex flex-col items-center">
+              <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white hover:bg-purple-700 transition-all">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  ></path>
+                </svg>
+              </div>
+              <span className="text-sm text-gray-700">구매내역</span>
             </div>
-            <span className="text-sm text-gray-700">구매내역</span>
-          </div>
-          <div className=" flex flex-col items-center">
-            <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
+          </Link>
+          <Link href="/profile/loved">
+            <div className=" flex flex-col items-center">
+              <div className="w-14 h-14 flex items-center justify-center mb-2 bg-purple-500 rounded-full text-white hover:bg-purple-700 transition-all">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  ></path>
+                </svg>
+              </div>
+              <span className="text-sm text-gray-700">관심목록</span>
             </div>
-            <span className="text-sm text-gray-700">관심목록</span>
-          </div>
+          </Link>
         </div>
         <div className="mt-5 pt-5 border-t border-gray-300">
           <div className="flex items-center space-x-3">
